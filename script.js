d4589e7f83e44e090ed76a717c71651eb7f8bf48
20Ara <xjfakddl@gmail.com>
2026-09-25 15:34:41 +0900

    Add async function to fetch user data

--- script.js
+++ script.js
@@ -195,3 +195,12 @@ search.addEventListener("input", () => {
 
 renderReservations();
 cancelEditButton.hidden = true;
+
+async function getUser() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await response.json();
+
+  console.log(data);
+}
+
+getUser();
